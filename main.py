113201b52dcd09d7a01c0b83d7f81457cbef10c2
schips21 <tkpokidova@gmail.com>
2022-01-13 07:18:21 +0300

    собрала данные по Уфе

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ def parse_rest_comments(link):
 
 if __name__ == '__main__':
     core_link = "https://www.tripadvisor.ru"
-    init_link = core_link + '/Restaurants-g608957-Bryansk_Bryansk_Oblast_Central_Russia.html'
+    init_link = core_link + '/Restaurants-g298518-Ufa_Republic_of_Bashkortostan_Volga_District.html'
     parsed_links_for_all_rests = []
     results_comments = []
     results_restaurants = []
@@ -140,7 +140,7 @@ if __name__ == '__main__':
     df_rest_links.to_csv('rest_links.csv', index=False, header=False)
     print('Ссылки на рестораны успешно собраны')
 
-    rest_id = 1561
+    rest_id = 1923
     try:
         for link in parsed_links_for_all_rests:
             print('Собираем данные о ресторане № ' + rest_id.__str__() + ' ' + link)
