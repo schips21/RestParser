853dcc2cd73ca782c662fa35f4069d36cf59550e
schips21 <tkpokidova@gmail.com>
2022-01-17 16:21:11 +0300

    собрала информацию по Тольятти, обновила индекс

--- main.py
+++ main.py
@@ -130,7 +130,7 @@ def parse_rest_comments(link):
 
 if __name__ == '__main__':
     core_link = "https://www.tripadvisor.ru"
-    init_link = core_link + '/Restaurants-g673777-Izhevsk_Udmurt_Republic_Volga_District.html'
+    init_link = core_link + '/Restaurants-g665311-Tolyatti_Samara_Oblast_Volga_District.html '
     parsed_links_for_all_rests = []
     results_comments = []
     results_restaurants = []
@@ -140,7 +140,7 @@ if __name__ == '__main__':
     df_rest_links.to_csv('rest_links.csv', index=False, header=False)
     print('Ссылки на рестораны успешно собраны')
 
-    rest_id = 7569
+    rest_id = 8051
     try:
         for link in parsed_links_for_all_rests:
             print('Собираем данные о ресторане № ' + rest_id.__str__() + ' ' + link)
